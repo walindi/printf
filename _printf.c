65f52877fe4325ecb3cdcd9478e9f53fc9534c54
walindi <walindibarack@yahoo.com>
2022-11-22 22:58:21 +0300

    Print function

---
+++ _printf.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _printf - print a char or a string
+ * @format: It's a character string
+ * Return: the number of character the function is printing
+ */
+int _printf(const char *format, ...)
+{
+	st_fmt st_format[] = {
+		{"d", print_num},
+		{"i", print_num}};
+
+	va_list list;
+	int count = 0;
+
+	va_start(list, format);
+	count =	get_match(format, list, st_format);
+	va_end(list);
+	return (count);
+}
