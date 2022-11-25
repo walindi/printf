cf71e2a609780a9f3c4cda50f661da57945ea2ed
walindi <walindibarack@yahoo.com>
2022-11-25 03:26:13 +0300

    Modified percent.c

--- percent.c
+++ percent.c
@@ -3,19 +3,16 @@
 /**
  * print_percent - print percentage string
  * @list: unused va_list parameter
- * Return: count of parameters
+ *
+ * Return: 1
  */
 
 int print_percent(__attribute__((unused))va_list list)
 {
 	char c = '%';
-	int count;
-
-	count = 0;
 
 	_putchar(c);
-	count++;
 
-	return (count);
+	return (1);
 
 }
